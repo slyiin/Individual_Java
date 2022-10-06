7ec2e9e5bfb91ce41c6746d7b1ef21be9e4218af
slyiin <batalunaleslyj@gmail.com>
2022-10-06 10:04:49 +0800

    food added

---
+++ Food.java
@@ -0,0 +1,16 @@
+public class Food<Integer> {
+    private String name;
+    private Integer obj;
+
+    public Food (Integer obj) {
+        this.obj = obj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

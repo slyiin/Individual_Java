7a2052efebbdc6668b6b8573f6cf55c7106e3d38
slyiin <112834519+slyiin@users.noreply.github.com>
2022-10-11 08:15:54 +0800

    Delete 2022-10-05 directory

--- 2022-10-05/MovieException.java
+++
@@ -1,9 +0,0 @@
-public class MovieException extends Exception {
-    public MovieException (String message) {
-        super(message);
-    }
-
-    public String notNumber() {
-        return "Your input is not a number.";
-    }
-}
